a03002688452b7b2acbae09d2d5e97a925156fea
Anatoliy <anatolikiselev@rambler.ru>
2023-08-03 12:52:43 +0300

    Решили Task6

--- HomeWork 1/Program.cs
+++ HomeWork 1/Program.cs
@@ -32,4 +32,15 @@ Console.Write ("Max number is " + max);
 */
 //Task 6. Напишите программу, которая на вход принимает число и выдает,
 //является ли число четным. 
-
+/*
+Console.Write ("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num%2 == 0)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+}
+*/
