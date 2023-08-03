83501053b27f12487fb99c054c8510e65c3433ae
Anatoliy <anatolikiselev@rambler.ru>
2023-08-03 11:19:53 +0300

    Записали условие Task4

--- HomeWork 1/Program.cs
+++ HomeWork 1/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿//Task 2. Напишите программу, которая на вход принимает два числа и выдает,
+//какое число больше, а какое меньше.
+/*
+Console.Write ("Input a first number: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Input a second number: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine ("Max number is " + num1); 
+    Console.WriteLine ("Min number is " + num2);
+}
+else
+{
+    Console.WriteLine ("Max number is " + num2);
+    Console.WriteLine ("Min number is " + num1);
+}
+*/
+//Task 4. Напишите в программу, которая принимает на вход три числа и выдает
+//максимальное из этих чисел.
+
