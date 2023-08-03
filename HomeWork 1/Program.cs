32aba9271ff1e618063b5d667050ff0f473d6536
Anatoliy <anatolikiselev@rambler.ru>
2023-08-03 12:39:02 +0300

    Решили Task4

--- HomeWork 1/Program.cs
+++ HomeWork 1/Program.cs
@@ -18,4 +18,17 @@ else
 */
 //Task 4. Напишите в программу, которая принимает на вход три числа и выдает
 //максимальное из этих чисел.
+/*
+Console.Write ("Input a first number: ");
+int num1 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Input a second number: ");
+int num2 = Convert.ToInt32 (Console.ReadLine());
+Console.Write ("Input a third number: ");
+int num3 = Convert.ToInt32 (Console.ReadLine());
+int max = num1;
+if (num2>max)max=num2;
+if (num3>max)max=num3;
+Console.Write ("Max number is " + max);
+*/
+
 
