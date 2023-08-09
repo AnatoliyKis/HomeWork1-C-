15820c0d827a305f8c751fca40b4595e353e8650
Anatoliy <anatolikiselev@rambler.ru>
2023-08-09 11:38:04 +0300

    Решили Task15

--- HomeWork 2/Program.cs
+++ HomeWork 2/Program.cs
@@ -14,7 +14,7 @@ Console.WriteLine($"New version of {num} is {NewNumber}");
 
 // Task 13. Напишите программу, которая выводит третью цифру заданного числа 
 //(или -1, если третьей цифры нет).
-
+/*
 int GetThirdRank (int num)
 {
     while (num>999)
@@ -39,3 +39,23 @@ if(CorrectNum(num))
 {
     Console.WriteLine($"Third digit of {num} is {NewNum}");
 }
+*/
+
+// Task 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+//и проверяет, является ли этот день выходным.
+/*
+bool HolidayCheck (int num)
+{
+    if(num==6||num==7)
+    return true;
+    else
+    {
+        return false;
+    }
+}
+Console.Write("Input number of day of the week: ");
+int num = Convert.ToInt32(Console.ReadLine());
+bool res =HolidayCheck (num);
+if(res) Console.WriteLine ("This day is a holiday");
+else Console.WriteLine ("This day is not a holiday");
+*/
