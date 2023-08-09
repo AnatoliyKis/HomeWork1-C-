b0abbd6dc8ba542197ae7b59382f483d538506ad
Anatoliy <anatolikiselev@rambler.ru>
2023-08-09 09:51:28 +0300

    Решили задачу Task10

---
+++ HomeWork 2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task 10. Напишите программу, которая на вход принимает трехзначное число, 
+//а на выходе показывает вторую цифру этого числа.
+
+int CutNumber(int num)
+{
+    int hundrads = num/10;
+    return hundrads%10;
+}
+Console.Write("Input three-digit number ");
+int num = Convert.ToInt32(Console.ReadLine());
+int NewNumber = CutNumber(num);
+Console.WriteLine($"New version of {num} is {NewNumber}");
