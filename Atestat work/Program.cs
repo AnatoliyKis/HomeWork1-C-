b8afa92f249ce2746950dcd565edf63668246dd2
Anatoliy <anatolikiselev@rambler.ru>
2023-09-08 10:51:40 +0300

    Решена задача 64

---
+++ Atestat work/Program.cs
@@ -0,0 +1,20 @@
+﻿//Решение задачи № 64//
+
+string PrintNumber(int num, string res)
+{
+    if(num<=1)
+    {
+        res=res+'1';
+        return res;
+    } 
+    else
+    {
+        res=res+num;
+        return PrintNumber(num-1, res);
+    }
+}
+string result= "";
+Console.Write("Input the number:  ");
+int number = Convert.ToInt32 (Console.ReadLine());
+result=PrintNumber (number, result);
+Console.WriteLine (result);
